5095129f8bb1ad1fa23b0e841444b1dd819f21f8
Akash Panigrahi <akash.panigrahi@vivaconnect.co>
2021-03-18 18:10:30 +0530

    convert normal query find to an aggregation pipeline

--- controllers/MovieController.js
+++ controllers/MovieController.js
@@ -4,36 +4,56 @@ const { query, validationResult } = require('express-validator');
 exports.movieList = [
     query('sort_by').optional().isIn(['99popularity', 'director', 'name']),
     query('order_by').optional().isIn(['asc', 'desc']),
-    query('genre').optional().isString(),
+    query('genres').optional().isString(),
     query('search').optional().isString().trim(),
+    query('start').optional().isString().toInt(),
     (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
 
-        const { sortBy = '_id', orderBy = 'asc', genre, search } = req.query;
-        const sortDirection = orderBy === 'desc' ? -1 : 1;
-        const findParams = {};
-        if (genre) {
-            findParams.genre = { $all: genre.split(',') };
+        const { sortBy = '_id', orderBy = 'asc', genres, search, start = 0 } = req.query;
+
+        const matchParams = {};
+        if (genres) {
+            matchParams.genre = { $all: genres.split(',') };
         }
         if (search) {
             const searchFilter = { $regex: search, $options: 'i' };
-            findParams.$or = [{ name: searchFilter }, { director: searchFilter }];
+            matchParams.$or = [{ name: searchFilter }, { director: searchFilter }];
         }
+
+        const pipeline = [];
+        if (genres || search) {
+            pipeline.push({ $match: matchParams });
+        }
+
+        const moviesFacet = [];
+        if (sortBy) {
+            moviesFacet.push({ $sort: { [sortBy]: orderBy === 'desc' ? -1 : 1 }});
+        }
+        if (start) {
+            moviesFacet.push({ $skip: start })
+        }
+        moviesFacet.push({ $limit: 20 });
+
+        pipeline.push({ $facet: {
+            movies: moviesFacet,
+            totalMovies: [{ $group: { _id: null, total: { $sum: 1 } } }]
+        }});
 
         process.mongo
             .db('imdb')
             .collection('movies')
-            .find(findParams)
-            .sort({ [sortBy]: sortDirection })
+            .aggregate(pipeline)
             .toArray((err, docs) => {
                 if (err) {
                     return res.status(500).send(err.message);
                 }
 
-                res.json(docs);
+                const { movies, totalMovies } = docs[0];
+                res.json({ movies, totalMovies: totalMovies[0] ? totalMovies[0].total : 0 });
             });
     }];
 
